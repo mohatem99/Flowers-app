1045b27fabb10092ee6bc40d32207dbc6b4c70d9
Mohamed Elsayed <mohamed.madoo2016@gmail.com>
2025-01-23 22:47:43 +0200

    react-query-setup

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -1,17 +1,22 @@
 import { NextIntlClientProvider, useMessages } from "next-intl";
 import NextAuthProvider from "./components/nextAuthProvider";
+import ReactQueryProvider from "./components/react-query-provider";
 type ProvidersProps = {
     children: React.ReactNode;
 };
 
 export default function Providers({ children }: ProvidersProps) {
-    // Translation
+
+
+
     const messages = useMessages();
 
     return (
         <NextIntlClientProvider messages={messages}>
             <NextAuthProvider>
+                <ReactQueryProvider>
                 {children}
+                </ReactQueryProvider>
             </NextAuthProvider>
         </NextIntlClientProvider>
     );
